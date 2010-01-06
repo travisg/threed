a337f3a3fe57c779d885b8645a4946c041457a1a
Travis Geiselbrecht <geist@foobox.com>
2010-01-06 11:07:48 +0000

    remove the old pointer based gl routine resolution
    git-svn-id: svn+ssh://svn.tkgeisel.com/var/svn/mysrc@836 0e4dd39d-42fa-0310-8bbd-9655faafb336

--- client/renderer/OpenGL/glinc.h
+++ client/renderer/OpenGL/glinc.h
@@ -28,63 +28,6 @@
 #error I dont know what platform I am!
 #endif
 
-#if defined(GL_EXT_POINTERS)
-
-/* buffer objects (GL 1.5) */
-extern PFNGLBINDBUFFERPROC glBindBuffer;
-extern PFNGLGENBUFFERSPROC glGenBuffers;
-extern PFNGLBUFFERDATAPROC glBufferData;
-extern PFNGLMAPBUFFERPROC  glMapBuffer;
-extern PFNGLUNMAPBUFFERPROC glUnmapBuffer;
-extern PFNGLDELETEBUFFERSPROC glDeleteBuffers;
-
-/* shader stuff */
-extern PFNGLCREATEPROGRAMPROC glCreateProgram;
-extern PFNGLDELETEPROGRAMPROC glDeleteProgram;
-extern PFNGLCREATESHADERPROC glCreateShader;
-extern PFNGLDELETESHADERPROC glDeleteShader;
-extern PFNGLDETACHSHADERPROC glDetachShader;
-extern PFNGLATTACHSHADERPROC glAttachShader;
-extern PFNGLSHADERSOURCEPROC glShaderSource;
-extern PFNGLGETSHADERSOURCEPROC glGetShaderSource;
-extern PFNGLCOMPILESHADERPROC glCompileShader;
-extern PFNGLLINKPROGRAMPROC glLinkProgram;
-extern PFNGLUSEPROGRAMPROC glUseProgram;
-extern PFNGLVALIDATEPROGRAMPROC glValidateProgram;
-extern PFNGLGETSHADERIVPROC glGetShaderiv;
-extern PFNGLGETPROGRAMIVPROC glGetProgramiv;
-extern PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog;
-extern PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
-extern PFNGLGETACTIVEUNIFORMPROC glGetActiveUniform;
-extern PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation;
-
-extern PFNGLUNIFORM1FPROC glUniform1f;
-extern PFNGLUNIFORM2FPROC glUniform2f;
-extern PFNGLUNIFORM3FPROC glUniform3f;
-extern PFNGLUNIFORM4FPROC glUniform4f;
-extern PFNGLUNIFORM1IPROC glUniform1i;
-extern PFNGLUNIFORM2IPROC glUniform2i;
-extern PFNGLUNIFORM3IPROC glUniform3i;
-extern PFNGLUNIFORM4IPROC glUniform4i;
-extern PFNGLUNIFORM1FVPROC glUniform1fv;
-extern PFNGLUNIFORM2FVPROC glUniform2fv;
-extern PFNGLUNIFORM3FVPROC glUniform3fv;
-extern PFNGLUNIFORM4FVPROC glUniform4fv;
-extern PFNGLUNIFORM1IVPROC glUniform1iv;
-extern PFNGLUNIFORM2IVPROC glUniform2iv;
-extern PFNGLUNIFORM3IVPROC glUniform3iv;
-extern PFNGLUNIFORM4IVPROC glUniform4iv;
-extern PFNGLUNIFORMMATRIX2FVPROC glUniformMatrix2fv;
-extern PFNGLUNIFORMMATRIX3FVPROC glUniformMatrix3fv;
-extern PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv;
-extern PFNGLGETUNIFORMFVPROC glGetUniformfv;
-extern PFNGLGETUNIFORMIVPROC glGetUniformiv;
-
-extern PFNGLACTIVETEXTUREPROC glActiveTexture;
-extern PFNGLCLIENTACTIVETEXTUREPROC glClientActiveTexture;
-
-#endif
-
 void InitGLExt();
 
 #endif
